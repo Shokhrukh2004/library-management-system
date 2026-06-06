1361c7b362c21cddc76f0fbc52e916cf518df9b9
Shokhrukh20004 <shoxruxernazarov215@gmail.com>
2026-06-06 17:13:03 +0200

    changed - database credentials stored in environment variables instead of properties file to secure them.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,7 @@ package org.example;
 import org.example.config.AppConfig;
 import org.example.ui.UInterface;
 import org.example.ui.cli.LibraryCLI;
-import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
