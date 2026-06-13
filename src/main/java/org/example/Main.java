ed4abe161555b298b04ed1bd269e48e49037deff
Shokhrukh20004 <shoxruxernazarov215@gmail.com>
2026-06-13 16:20:00 +0200

    changed - main class from manual application context creation to spring boot

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,12 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.example.config.AppConfig;
-import org.example.ui.UInterface;
-import org.example.ui.cli.LibraryCLI;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-
-        UInterface ui = context.getBean(LibraryCLI.class);
-        ui.run();
-
-        context.close();
+        SpringApplication.run(Main.class, args);
     }
 }
